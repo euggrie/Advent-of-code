44ff838f7d4090aa97abe900690dc2e471eee4e2
Eugenia Grieff <eugenia@Eugenias-MacBook-Pro.local>
2016-03-22 15:32:54 +0000

    Partial 3b

--- 3b.rb
+++ 3b.rb
@@ -1,35 +1,52 @@
-dir = "^v^v^v^v^v"
+dir = "v>v<vvv<<vv^v<v>vv>v<<<^^^^^<<^<vv>^>v^>^"
 
 
 def calculate_presents(arr)
-  count = []
-  location = {x: 0, y: 0}
 
-  north = "^"
-  south = "v"
-  east = ">"
-  west = "<"
+  santa = []
+  bot = []
+  arr3 = []
+  santa_location = {x: 0, y: 0}
+  bot_location = {x: 0, y: 0}
+
+
 
   arr = arr.split("")
-  arr.each do |i|
-    if i == north
-      location[:y] += 1
-      count.push(location.dup)
-    elsif i == south
-      location[:y] -= 1
-      count.push(location.dup)
-    elsif i == east
-      location[:x] += 1
-      count.push(location.dup)
-    else
-      location[:x] -= 1
-      count.push(location.dup)
+  santa =  arr.values_at(* arr.each_index.select {|i| i.even?})
+  bot = arr.values_at(* arr.each_index.select {|i| i.odd?})
+
+  def calculate_location(instructions, location)
+    count = []
+    north = "^"
+    south = "v"
+    east = ">"
+    west = "<"
+    instructions.each do |i|
+      if i == north
+        location[:y] += 1
+        count.push(location.dup)
+      elsif i == south
+        location[:y] -= 1
+        count.push(location.dup)
+      elsif i == east
+        location[:x] += 1
+        count.push(location.dup)
+      else
+        location[:x] -= 1
+        count.push(location.dup)
+      end
+
     end
-
+    return count
+    #print count.uniq.length
   end
+  arr1 = calculate_location(santa, santa_location)
+  arr2 = calculate_location(bot, bot_location)
 
-  count.uniq.length
-
+  arr1.each do |z| arr2.include?(z)
+    puts "hurra"
+    arr3.push(z)
+  end
 end
 
-puts calculate_presents(dir)
+calculate_presents(dir)
